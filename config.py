2335798fe9b582a410833e377a710ab9c1bb9bdd
gopaljibillionaire-dotcom <gopaljibillionaire@gmail.com>
2026-07-16 22:09:14 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+API_ID = int(os.getenv("TG_API_ID", "30636134"))
+API_HASH = os.getenv("TG_API_HASH", "9c5bb2bbeb19a0da5bfb0e7052875d2f")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8733721396:AAHJrr4uHC2WEx5r6BCHqBmx4LbMKh1Ngds")
+
+SUPER_OWNER_IDS = [7952327997, 7953147643, 8064493735] 
+LOG_GC_ID = -1004349607766  
+SECRET_KEY = os.getenv("ENCRYPTION_KEY", "pydroid_secure_fallback_key_2026")
